98c5b38dbfd40510ba4e635163c89101a521fcba
erickorlando <erickorlando@outlook.com>
2015-09-08 20:30:30 -0500

    Se termina el juego de pokemon

--- js/pokemones.js
+++ js/pokemones.js
@@ -1,3 +1,24 @@
+function Pokemon(n,v,a)
+{
+    this.grito = "Pika!";
+    this.nombre = n;
+    this.vida = v;
+    this.ataque = a;
+    this.gritar = function()
+    {
+        alert(this.grito);
+    }
+}
+
+function inicio(){
+    var rattata = new Pokemon("Rattata",40,2);
+    rattata.vida = rattata.vida - 13;
+    rattata.grito = "Ola ke ase!";
+    
+    nombrePokemon.textContent = rattata.nombre;
+}
+
+/*
 function Pokemon(nombrePokemon, vidaPoke, ataPoke) {
 	var estructuraPokemon = {
 		nombre: nombrePokemon,
@@ -10,5 +31,5 @@ function Pokemon(nombrePokemon, vidaPoke, ataPoke) {
 var pikachu = Pokemon("Pikachu", 100,55);
 var bulbasaur = Pokemon("Bulbasaur",90,50);
 
-document.writeln(bulbasaur.nombre);
-document.write(pikachu.nombre);
+console.log(bulbasaur);
+*/
