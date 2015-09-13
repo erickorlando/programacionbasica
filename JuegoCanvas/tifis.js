369942e38d90a0ab9a912e40eea46c2404e2cab2
erickorlando <erickorlando@outlook.com>
2015-09-13 14:02:37 -0500

    Se añade archivos del Juego en Canvas

---
+++ JuegoCanvas/tifis.js
@@ -0,0 +1,9 @@
+var tablero;
+var fondo = {
+    imagenURL: "fondo.png"
+};
+function inicio() {
+    var canvas = document.getElementById("campo");
+    tablero = canvas.getContext("2d");
+    fon
+}
