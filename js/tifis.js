8f49540f0a81ab076520eb7e6821835764b9941a
erickorlando <erickorlando@outlook.com>
2015-09-14 12:52:44 -0500

    Se añade otra funcion de dibujo

--- js/tifis.js
+++ js/tifis.js
@@ -1,14 +1,31 @@
 var tablero;
 var fondo = {
+    imagenOK: false,
     imagenURL: "Images/fondo.png"
 };
+
+var tifis = {
+    x: 200,
+    y: 100,
+    frenteURL: "Images/diana-frente.png",
+    frenteOK: false
+}
+
 function juegoCanvasInicio() {
     var canvas = document.getElementById("campo");
     tablero = canvas.getContext("2d");
 
     fondo.imagen = new Image();
     fondo.imagen.src = fondo.imagenURL;
-    fondo.imagen.onload = function () {
-        tablero.drawImage(fondo.imagen, 0, 0);
-    };
+    fondo.imagen.onload = confirmarFondo;
+}
+function confirmarFondo(){
+    fondo.imagenOK = true;
+    dibujar();
+}
+function dibujar(){
+    if (fondo.imagenOK == true)
+    {
+        tablero.drawImage(fondo.imagen,0,0);
+    }
 }
